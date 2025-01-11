6ec1a0e9f0c0c8612492e19027c182bac46b57f1
Robert Soncrant <devreaper0@gmail.com>
2025-01-10 22:46:45 -0600

    feat!: multiple instances other than the default global one

--- taurus.go
+++ taurus.go
@@ -12,54 +12,93 @@ import (
 	"github.com/spf13/pflag"
 )
 
-var (
+var t *Taurus
+
+func init() {
+	t = New()
+}
+
+type Taurus struct {
 	envPrefix          string
 	expandEnv          bool
-	envAliases         = map[string][]string{}
-	flags              = map[string]*pflag.Flag{}
-	customMarshalers   = map[reflect.Type]func(interface{}) ([]byte, error){}
-	customUnmarshalers = map[reflect.Type]func(interface{}, []byte) error{}
-)
+	envAliases         map[string][]string
+	flags              map[string]*pflag.Flag
+	customMarshalers   map[reflect.Type]func(interface{}) ([]byte, error)
+	customUnmarshalers map[reflect.Type]func(interface{}, []byte) error
+}
+
+func New() *Taurus {
+	return &Taurus{
+		envAliases:         make(map[string][]string),
+		flags:              make(map[string]*pflag.Flag),
+		customMarshalers:   make(map[reflect.Type]func(interface{}) ([]byte, error)),
+		customUnmarshalers: make(map[reflect.Type]func(interface{}, []byte) error),
+	}
+}
 
 func SetEnvPrefix(prefix string) {
-	envPrefix = prefix
+	t.SetEnvPrefix(prefix)
+}
+
+func (t *Taurus) SetEnvPrefix(prefix string) {
+	t.envPrefix = prefix
 }
 
 func SetExpandEnv(expand bool) {
-	expandEnv = expand
+	t.SetExpandEnv(expand)
+}
+
+func (t *Taurus) SetExpandEnv(expand bool) {
+	t.expandEnv = expand
 }
 
 func BindEnvAlias(fieldPath string, aliases ...string) {
-	envAliases[fieldPath] = append(envAliases[fieldPath], aliases...)
+	t.BindEnvAlias(fieldPath, aliases...)
+}
+
+func (t *Taurus) BindEnvAlias(fieldPath string, aliases ...string) {
+	t.envAliases[fieldPath] = append(t.envAliases[fieldPath], aliases...)
 }
 
 func BindFlag(fieldPath string, flag *pflag.Flag) {
-	flags[fieldPath] = flag
+	t.BindFlag(fieldPath, flag)
 }
 
-func RegisterCustomMarshaler[T any](marshaler func(T) ([]byte, error)) {
-	customMarshalers[reflect.TypeFor[T]()] = func(v interface{}) ([]byte, error) {
+func (t *Taurus) BindFlag(fieldPath string, flag *pflag.Flag) {
+	t.flags[fieldPath] = flag
+}
+
+// RegisterCustomMarshaler registers a custom marshaler for the type argument T.
+// If the parameter "t" is nil, the default Taurus instance is used.
+func RegisterCustomMarshaler[T any](t *Taurus, marshaler func(T) ([]byte, error)) {
+	t.customMarshalers[reflect.TypeFor[T]()] = func(v interface{}) ([]byte, error) {
 		return marshaler(v.(T))
 	}
 
-	yaml.RegisterCustomMarshaler(func(t T) ([]byte, error) {
-		return marshaler(t)
+	yaml.RegisterCustomMarshaler(func(v T) ([]byte, error) {
+		return marshaler(v)
 	})
 }
 
-func RegisterCustomUnmarshaler[T any](unmarshaler func(*T, []byte) error) {
-	customUnmarshalers[reflect.TypeFor[T]()] = func(v interface{}, data []byte) error {
+// RegisterCustomUnmarshaler registers a custom unmarshaler for the type argument T.
+// If the parameter "t" is nil, the default Taurus instance is used.
+func RegisterCustomUnmarshaler[T any](t *Taurus, unmarshaler func(*T, []byte) error) {
+	t.customUnmarshalers[reflect.TypeFor[T]()] = func(v interface{}, data []byte) error {
 		return unmarshaler(v.(*T), data)
 	}
 
-	yaml.RegisterCustomUnmarshaler(func(t *T, data []byte) error {
-		return unmarshaler(t, data)
+	yaml.RegisterCustomUnmarshaler(func(v *T, data []byte) error {
+		return unmarshaler(v, data)
 	})
 }
 
 func Load(configData string, cfg interface{}) error {
+	return t.Load(configData, cfg)
+}
+
+func (t *Taurus) Load(configData string, cfg interface{}) error {
 	data := configData
-	if expandEnv {
+	if t.expandEnv {
 		data = os.ExpandEnv(data)
 	}
 
@@ -70,24 +109,32 @@ func Load(configData string, cfg interface{}) error {
 }
 
 func LoadFile(configFilePath string, cfg interface{}) error {
+	return t.LoadFile(configFilePath, cfg)
+}
+
+func (t *Taurus) LoadFile(configFilePath string, cfg interface{}) error {
 	configData, err := os.ReadFile(configFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to read config file: %w", err)
 	}
-	return Load(string(configData), cfg)
+	return t.Load(string(configData), cfg)
 }
 
 func LoadEnv(prefix string, cfg interface{}) error {
+	return t.LoadEnv(prefix, cfg)
+}
+
+func (t *Taurus) LoadEnv(prefix string, cfg interface{}) error {
 	v := reflect.ValueOf(cfg)
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
 		return fmt.Errorf("cfg must be a pointer to a struct")
 	}
 
 	v = v.Elem()
-	t := v.Type()
+	ty := v.Type()
 
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
+	for i := 0; i < ty.NumField(); i++ {
+		field := ty.Field(i)
 		fieldValue := v.Field(i)
 
 		// Get the `yaml` tag or default to field name
@@ -113,10 +160,10 @@ func LoadEnv(prefix string, cfg interface{}) error {
 
 		envVal, exists := os.LookupEnv(envKey)
 		if !exists {
-			for _, key := range envAliases[strings.TrimPrefix(envKey, envPrefix+"_")] {
-				envVal, exists = os.LookupEnv(envPrefix + "_" + key)
+			for _, key := range t.envAliases[strings.TrimPrefix(envKey, t.envPrefix+"_")] {
+				envVal, exists = os.LookupEnv(t.envPrefix + "_" + key)
 				if exists {
-					envKey = envPrefix + "_" + key
+					envKey = t.envPrefix + "_" + key
 					break
 				}
 			}
@@ -125,7 +172,7 @@ func LoadEnv(prefix string, cfg interface{}) error {
 			continue
 		}
 
-		if err := setField(field, fieldValue, envVal); err != nil {
+		if err := t.setField(field, fieldValue, envVal); err != nil {
 			return fmt.Errorf("error setting field for %s: %v", envKey, err)
 		}
 	}
@@ -133,20 +180,24 @@ func LoadEnv(prefix string, cfg interface{}) error {
 }
 
 func LoadFlags(cfg interface{}) error {
-	return loadFlags("", cfg)
+	return t.LoadFlags(cfg)
 }
 
-func loadFlags(prefix string, cfg interface{}) error {
+func (t *Taurus) LoadFlags(cfg interface{}) error {
+	return t.loadFlags("", cfg)
+}
+
+func (t *Taurus) loadFlags(prefix string, cfg interface{}) error {
 	v := reflect.ValueOf(cfg)
 	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
 		return fmt.Errorf("cfg must be a pointer to a struct")
 	}
 
 	v = v.Elem()
-	t := v.Type()
+	ty := v.Type()
 
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
+	for i := 0; i < ty.NumField(); i++ {
+		field := ty.Field(i)
 		fieldValue := v.Field(i)
 
 		fieldPath := field.Name
@@ -155,7 +206,7 @@ func loadFlags(prefix string, cfg interface{}) error {
 		}
 
 		if field.Type.Kind() == reflect.Struct {
-			if err := loadFlags(fieldPath, fieldValue.Addr().Interface()); err != nil {
+			if err := t.loadFlags(fieldPath, fieldValue.Addr().Interface()); err != nil {
 				return err
 			}
 			continue
@@ -165,20 +216,20 @@ func loadFlags(prefix string, cfg interface{}) error {
 			continue
 		}
 
-		fl, exists := flags[fieldPath]
+		fl, exists := t.flags[fieldPath]
 		if !exists || !fl.Changed {
 			continue
 		}
 
-		if err := setField(field, fieldValue, fl.Value.String()); err != nil {
+		if err := t.setField(field, fieldValue, fl.Value.String()); err != nil {
 			return fmt.Errorf("error setting field for %s: %v", fieldPath, err)
 		}
 	}
 	return nil
 }
 
-func setField(field reflect.StructField, fieldValue reflect.Value, value string) error {
-	if customUnmarshaler, exists := customUnmarshalers[field.Type]; exists {
+func (t *Taurus) setField(field reflect.StructField, fieldValue reflect.Value, value string) error {
+	if customUnmarshaler, exists := t.customUnmarshalers[field.Type]; exists {
 		if err := customUnmarshaler(fieldValue.Addr().Interface(), []byte(value)); err != nil {
 			return fmt.Errorf("failed to unmarshal custom field: %v", err)
 		}
